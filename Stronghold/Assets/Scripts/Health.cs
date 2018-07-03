35429eab4031b2c13d95f56a09c8763ad17fbd39
Víctor Labayen <victor.mendi3216@gmail.com>
2018-07-03 18:21:25 +0200

    Basic building
    health: sow and get damag

---
+++ Stronghold/Assets/Scripts/Health.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	private int hp;
+	private int maxHP;
+	public HealthBarController HealthBar;
+
+	//temporal *************************
+	void Start(){
+		setup(400);
+	}
+	void Update(){
+		getDmg(1);
+	}
+	//******************************
+
+
+	//Se requiere hp maximo (e inicial)
+	public void setup(int _maxHP){
+		hp = _maxHP;
+		maxHP = _maxHP;
+	}
+
+	//Get damage over hp field and update healthbar
+	public void getDmg(int amount){
+		hp -= amount;
+		HealthBar.setHP(hp, maxHP);
+
+		// if (hp < 0){
+		// 	die
+		// }
+	}
+}
---
+++ Stronghold/Assets/Scripts/HealthBarController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour {
+
+	[SerializeField]
+	private RectTransform healthBar;
+
+	//Update showing health
+	public void setHP(int HP, int maxHP){
+		healthBar.sizeDelta = new Vector2(100 * HP / (float) maxHP, 0);
+	}
+
+}
